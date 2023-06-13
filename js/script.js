a505ad745bb872af5a1a1843c3c8461932d6ac49
Muhibbur Rahman <82113669+Muhib68442@users.noreply.github.com>
2023-06-13 23:26:35 +0600

    Added 4 more Categories !
    - Buy Accessories
    - Personalize
    - Tutorial
    - Suggestion
    
    *Bugs Fixed
    *Debug incomplete
    *Features Added
    
    v 1.0.3

--- js/script.js
+++ js/script.js
@@ -1,113 +1,102 @@
 // Click Text To Copy
 const copyTextElements = document.querySelectorAll('.copy-text');
 
-    copyTextElements.forEach(function(element) {
-        element.addEventListener('click', function() {
-            const selection = window.getSelection();
-            const range = document.createRange();
-            range.selectNodeContents(element);
-            selection.removeAllRanges();
-            selection.addRange(range);
-            document.execCommand('copy');
-            alert('Command copied to clipboard!');
-        });
-    });
+copyTextElements.forEach(function (element) {
+  element.addEventListener('click', function () {
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNodeContents(element);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand('copy');
+    alert('Command copied to clipboard!');
+  });
+});
 
 
-    // SEARCH 
+// SEARCH 
 
-    // function searchFunction() {
-    //     let input = document.querySelector('.search-input').value;
-        
-    //     // get all the cards, category titles and card wrappers on the page
-    //     let cards = document.querySelectorAll('.card');
-    //     let categoryTitles = document.querySelectorAll('.card-category-title');
-    //     let cardWrappers = document.querySelectorAll('.card-wrapper');
-        
-    //     // hide all the category titles and card wrappers
-    //     for (let title of categoryTitles) {
-    //       title.style.display = 'none';
-    //     }
-    //     for (let wrapper of cardWrappers) {
-    //       wrapper.style.display = 'none';
-    //     }
-        
-    //     // loop through the cards and check if the app name matches the search input
-    //     for (let card of cards) {
-    //       let appName = card.querySelector('.card-name').textContent;
-    //       if (appName.toLowerCase().includes(input.toLowerCase())) {
-    //         // app name matches the search input
-    //         // do something with the matching card
-    //         card.style.display = 'block';
-    //         card.parentNode.style.display = 'flex';
-    //         card.parentNode.style.justifyContent = 'center';
-    //       } else {
-    //         // app name does not match the search input
-    //         // hide the card
-    //         card.style.display = 'none';
-    //       }
-    //     }
-    //   }
+function searchFunction() {
+  let input = document.querySelector('.search-input').value;
+
+  let cards = document.querySelectorAll('.card');
+  let categoryTitles = document.querySelectorAll('.card-category-title');
+  let cardWrappers = document.querySelectorAll('.card-wrapper');
+
+  for (let title of categoryTitles) {
+    title.style.display = 'none';
+  }
+  for (let wrapper of cardWrappers) {
+    wrapper.style.display = 'none';
+  }
+
+  // loop through the cards and check if the app name matches the search input
+  for (let card of cards) {
+    let appName = card.querySelector('.card-name').textContent;
+    if (appName.toLowerCase().includes(input.toLowerCase())) {
+
+      card.style.display = 'block';
+      card.parentNode.style.display = 'flex';
+      card.parentNode.style.justifyContent = 'center';
+    } else {
+
+      card.style.display = 'none';
+    }
+  }
+}
+
+function closeFunction() {
+  // clear the search input
+  document.querySelector('.search-input').value = '';
+
+  // show all the cards, category titles and card wrappers
+  let cards = document.querySelectorAll('.card');
+  let categoryTitles = document.querySelectorAll('.card-category-title');
+  let cardWrappers = document.querySelectorAll('.card-wrapper');
+
+  for (let title of categoryTitles) {
+    title.style.display = 'block';
+  }
+  for (let wrapper of cardWrappers) {
+    wrapper.style.display = 'flex';
+    wrapper.style.justifyContent = 'space-between';
+  }
+  for (let card of cards) {
+    card.style.display = 'block';
+  }
+}
+
+function handleEnterKey(event) {
+  if (event.keyCode === 13) {
+    // user pressed the Enter key
+    // call the searchFunction
+    searchFunction();
+  }
+}
 
 
-    function searchFunction() {
-        let input = document.querySelector('.search-input').value;
-        
-        // get all the cards, category titles and card wrappers on the page
-        let cards = document.querySelectorAll('.card');
-        let categoryTitles = document.querySelectorAll('.card-category-title');
-        let cardWrappers = document.querySelectorAll('.card-wrapper');
-        
-        // hide all the category titles and card wrappers
-        for (let title of categoryTitles) {
-          title.style.display = 'none';
-        }
-        for (let wrapper of cardWrappers) {
-          wrapper.style.display = 'none';
-        }
-        
-        // loop through the cards and check if the app name matches the search input
-        for (let card of cards) {
-          let appName = card.querySelector('.card-name').textContent;
-          if (appName.toLowerCase().includes(input.toLowerCase())) {
-            // app name matches the search input
-            // do something with the matching card
-            card.style.display = 'block';
-            card.parentNode.style.display = 'flex';
-            card.parentNode.style.justifyContent = 'center';
-          } else {
-            // app name does not match the search input
-            // hide the card
-            card.style.display = 'none';
-          }
-        }
-      }
-      
-      function closeFunction() {
-        // clear the search input
-        document.querySelector('.search-input').value = '';
-        
-        // show all the cards, category titles and card wrappers
-        let cards = document.querySelectorAll('.card');
-        let categoryTitles = document.querySelectorAll('.card-category-title');
-        let cardWrappers = document.querySelectorAll('.card-wrapper');
-        
-        for (let title of categoryTitles) {
-          title.style.display = 'block';
-        }
-        for (let wrapper of cardWrappers) {
-          wrapper.style.display = 'flex';
-          wrapper.style.justifyContent = 'space-between';
-        }
-        for (let card of cards) {
-          card.style.display = 'block';
-        }
-      }
-      
-      function handleEnterKey(event) {
-        if (event.keyCode === 13) {
-          // user pressed the Enter key
-          // call the searchFunction
-          searchFunction();
-        }
-      }
+
+/* Click to show Wallpaper or Icon (Switcher) */
+
+// function showContent(section) {
+//   document.querySelectorAll('.content-section').forEach(function(el) {
+//       el.style.display = 'none';
+//   });
+//   document.getElementById(section).style.display = 'block';
+
+//   document.querySelector('.personalize-show-default').style.display = 'none';
+// }
+
+function showContent(section, link) {
+  document.querySelectorAll('.content-section').forEach(function(el) {
+      el.style.display = 'none';
+  });
+  document.getElementById(section).style.display = 'block';
+
+  document.querySelector('.personalize-show-default').style.display = 'none';
+
+  document.querySelectorAll('.switcher-div a').forEach(function(el) {
+      el.classList.remove('active');
+  });
+  link.classList.add('active');
+}
